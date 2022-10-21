a38f8dfca2f8a2932df938a86b2548e0f7933503
Nav <nmatharu338@gmail.com>
2022-10-21 11:33:05 -0700

    Add variable number of HP bar notches

--- Assets/Scripts/GameUI/PlayerStatusBar.cs
+++ Assets/Scripts/GameUI/PlayerStatusBar.cs
@@ -10,6 +10,14 @@ public class PlayerStatusBar : MonoBehaviour
     [ SerializeField ] Image hpBar;
     [ SerializeField ] Image hpFollowBar;
 
+    [ SerializeField ] Transform notchesParent;
+    [ SerializeField ] GameObject notch;
+    [ SerializeField ] int notchDivision = 10;
+    [ SerializeField ] float maxNotchWidth = 0.08f;
+    [ SerializeField ] float minNotchWidth = 0.04f;
+    [ SerializeField ] int maxNotchWidthThreshold = 50;
+    [ SerializeField ] int minNotchWidthThreshold = 200;
+    
     const float HpFollowBarSpeed = 0.6f;
     // We Want the follow bar to scale with max hp, so higher healths take longer to go down
     const float HpFollowBarSpeedDivisor = 18;
@@ -27,12 +35,11 @@ public class PlayerStatusBar : MonoBehaviour
         _hpPct = _player.HpPct();
         _hpFollowPct = _hpPct;
         
-        
-        // TODO Set HealthBar UI Notches etc.
-        
         var sizeDelta = hpBar.rectTransform.sizeDelta;
         _maxHpBarWidth = sizeDelta.x;
         _hpBarHeight = sizeDelta.y;
+        
+        SetHpBarNotches();
     }
 
     void Update()
@@ -41,5 +48,23 @@ public class PlayerStatusBar : MonoBehaviour
         _hpFollowPct = Mathf.MoveTowards( _hpFollowPct, _hpPct, HpFollowBarSpeedDivisor / _maxHp * Time.deltaTime );
         hpBar.rectTransform.sizeDelta = new Vector2( _hpPct * _maxHpBarWidth, _hpBarHeight );
         hpFollowBar.rectTransform.sizeDelta = new Vector2( _hpFollowPct * _maxHpBarWidth, _hpBarHeight );
+    }
+
+    void SetHpBarNotches()
+    {
+        foreach( Transform n in notchesParent )
+            Destroy( n.gameObject );
+
+        var notchWidth = JBB.ClampedMap( _maxHp, 
+            minNotchWidthThreshold, maxNotchWidthThreshold,
+            minNotchWidth, maxNotchWidth );
+
+        for( var hp = notchDivision; hp < _maxHp; hp += notchDivision )
+        {
+            var image = Instantiate( notch, notchesParent ).GetComponent<Image>();
+            image.rectTransform.sizeDelta = new Vector2( notchWidth, _hpBarHeight );
+            image.rectTransform.anchoredPosition = 
+                new Vector3( hp / _maxHp * _maxHpBarWidth, 0, 0 );
+        }
     }
 }
