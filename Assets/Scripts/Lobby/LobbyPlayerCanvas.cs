e75e1de817b842d92e334e7cc65ffeb7e90fc215
Nav <nmatharu338@gmail.com>
2022-11-07 14:11:08 -0800

    Add generatable keyboard UI elements

--- Assets/Scripts/Lobby/LobbyPlayerCanvas.cs
+++ Assets/Scripts/Lobby/LobbyPlayerCanvas.cs
@@ -9,6 +9,14 @@ public class LobbyPlayerCanvas : MonoBehaviour
     [ SerializeField ] GameObject descCanvas;
     [ SerializeField ] TextMeshProUGUI playerName;
     [ SerializeField ] TextMeshProUGUI controllerName;
+
+    [ TextArea( 1, 2 ) ]
+    [ SerializeField ] string validNameCharacters;
+    [ SerializeField ] Vector3 typeCharStartPos;
+    [ SerializeField ] float typeCharDistDelta;
+    [ SerializeField ] int typeCharRowLength = 13;
+    [ SerializeField ] GameObject typeCharPrefab;
+    [ SerializeField ] Transform typeCharParent;
     
     void Start()
     {
@@ -22,5 +30,30 @@ public class LobbyPlayerCanvas : MonoBehaviour
         descCanvas.SetActive( true );
         controllerName.text = deviceName.ToUpper();
         playerName.text = pName;
+
+        PopulateCharacters();
+    }
+
+    void PopulateCharacters()
+    {
+        for( var i = 0; i < validNameCharacters.Length; i++ )
+        {
+            var x = i % typeCharRowLength;
+            var y = i / typeCharRowLength;
+            var o = Instantiate( typeCharPrefab, 
+                typeCharStartPos + typeCharDistDelta * new Vector3( x, -y, 0f ), 
+                Quaternion.identity );
+            o.GetComponent<RectTransform>().SetParent( typeCharParent, false );
+            o.GetComponent<LobbyTypeChar>().Init( validNameCharacters[ i ] );
+        }
+
+        for( var x = typeCharRowLength - 3; x < typeCharRowLength; x++ )
+        {
+            var o = Instantiate( typeCharPrefab,
+                typeCharStartPos + typeCharDistDelta * new Vector3( x, -3f, 0f ),
+                Quaternion.identity );
+            o.GetComponent<RectTransform>().SetParent( typeCharParent, false );
+            o.GetComponent<LobbyTypeChar>().InitSpecial( x - typeCharRowLength + 3 );
+        }
     }
 }
---
+++ Assets/Scripts/Lobby/LobbyTypeChar.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyTypeChar : MonoBehaviour
+{
+    [ SerializeField ] float unhoverAlpha = 0.5f;
+    [ SerializeField ] TextMeshProUGUI charDisplay;
+    [ SerializeField ] Image outlineBox;
+    [ SerializeField ] Image imageDisplay;
+    [ SerializeField ] Sprite[] sprites;
+    
+    char _display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Unhover();
+    }
+
+    public void Hover()
+    {
+        charDisplay.color = Color.white;
+        outlineBox.color = Color.white;
+        imageDisplay.color = Color.white;
+    }
+
+    public void Unhover()
+    {
+        charDisplay.color = new Color( 1f, 1f, 1f, unhoverAlpha );
+        outlineBox.color = new Color( 1f, 1f, 1f, unhoverAlpha );
+        imageDisplay.color = new Color( 1f, 1f, 1f, unhoverAlpha );
+    }
+
+    public void Init( char c )
+    {
+        _display = c;
+        charDisplay.text = _display.ToString();
+        imageDisplay.enabled = false;
+    }
+
+    public void InitSpecial( int x )
+    {
+        imageDisplay.sprite = sprites[ x ];
+        charDisplay.enabled = false;
+    }
+}
