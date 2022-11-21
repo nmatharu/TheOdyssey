5b25d649f5d76b90ed0fdcbb5f2a2ecdf851070c
Nav <nmatharu338@gmail.com>
2022-11-21 04:36:28 -0800

    Add end level lighting and block experimentation

---
+++ Assets/Scripts/WorldGen/LevelEnd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelEnd : Level
+{
+    public override void Generate( WorldGenerator generator ) {
+        environmentalParent.gameObject.SetActive( true );
+        generator.GenerateBase( baseBlock, DefaultWorldSizeX, DefaultWorldSizeY );
+        // generator.GrasslandsPub();
+        // generator.DuplicateTopRow( DefaultWorldSizeX, DefaultWorldSizeY );
+    }
+}
